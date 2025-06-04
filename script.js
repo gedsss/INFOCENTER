bdbbc5ba74ba066245ec79e55a0fbefde0879f6e
Gabriel <gabrieledsoon3@gmail.com>
2025-06-04 19:16:18 -0400

    Atualização do projeto.

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
 const modal = document.getElementById("modal");
 const modalImg = document.getElementById("modal-img");
 const modalInfo = document.querySelector(".modal-info p");
+const modalTitle = document.querySelector(".modal-title p");
 const closeBtn = document.querySelector(".modal-close");
 
-const images = document.querySelectorAll(".img-modal");
+const imagens = document.querySelectorAll(".img-modal");
 
 // biome-ignore lint/complexity/noForEach: <explanation>
-images.forEach(img => {
+imagens.forEach(img => {
   img.addEventListener("click", () => {
-    modal.style.display = "flex";
     modalImg.src = img.src;
+    modalTitle.textContent = img.getAttribute("data-title") || "Sem título";
     modalInfo.textContent = img.getAttribute("data-desc") || "Sem descrição disponível.";
+    modal.style.opacity = 0;
+    modal.style.display = "flex";
+    setTimeout(() => {
+      modal.style.opacity = 1;
+    }, 200);
   });
 });
 
 closeBtn.onclick = () => {
-  modal.style.display = "none";
+  modal.style.opacity = 0;
+  setTimeout(() => {
+    modal.style.display = "none";
+  }, 400);
 };
 
 window.onclick = (event) => {
   if (event.target === modal) {
-    modal.style.display = "none";
+    modal.style.opacity = 0;
+    setTimeout(() => {
+      modal.style.display = "none";
+    }, 400);
   }
 };
